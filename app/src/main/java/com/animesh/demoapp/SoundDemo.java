759fc8f0c4cc68380d2b0efb9871a279c0fde6e0
An571sha <anim_esh@yahoo.co.in>
2019-04-04 10:47:33 +0300

    ++ Sound demo app has been changed to sync with system volume ++

--- app/src/main/java/com/animesh/demoapp/SoundDemo.java
+++ app/src/main/java/com/animesh/demoapp/SoundDemo.java
@@ -1,30 +1,38 @@
 package com.animesh.demoapp;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class SoundDemo extends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
     private AudioManager audio ;
     private int maxVol;
     private int curVol;
+    private SeekBar volumneControl;
+    private SeekBar musicControl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sounddemo);
         mMediaPlayer = MediaPlayer.create(this, R.raw.bell);
+
         audio =(AudioManager)getSystemService(Context.AUDIO_SERVICE);
         maxVol = audio.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         curVol = audio.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-        final SeekBar volumneControl =(SeekBar) findViewById(R.id.seekBar);
+        volumneControl = findViewById(R.id.seekBar);
         volumneControl.setMax(maxVol);
         volumneControl.setProgress(curVol);
 
@@ -32,7 +40,7 @@ public class SoundDemo extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int p, boolean b) {
                 Log.i("SeekBar Value", Integer.toString(p));
-                audio.setStreamVolume(AudioManager.STREAM_MUSIC,p,0);
+                audio.setStreamVolume(AudioManager.STREAM_MUSIC,p,AudioManager.FLAG_SHOW_UI);
             }
 
             @Override
@@ -45,6 +53,35 @@ public class SoundDemo extends AppCompatActivity {
 
             }
         });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                musicControl.setProgress(mMediaPlayer.getCurrentPosition());
+            }
+        },0, 1000);
+
+
+        musicControl = (SeekBar) findViewById(R.id.musicSeek);
+        musicControl.setMax(mMediaPlayer.getDuration());
+        musicControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                mMediaPlayer.seekTo(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
     public void play(View view){
@@ -53,7 +90,12 @@ public class SoundDemo extends AppCompatActivity {
     }
 
     public void pause(View view){
+
         mMediaPlayer.pause();
     }
+
+
+
+
 
 }
