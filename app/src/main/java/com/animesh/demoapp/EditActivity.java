7e54c6540b84adae84f7f11d20dffb4185d818cb
An571sha <anim_esh@yahoo.co.in>
2019-04-11 10:50:12 +0300

    ++ NotesApp can now store and delete entries using sharedPref ++

--- app/src/main/java/com/animesh/demoapp/EditActivity.java
+++ app/src/main/java/com/animesh/demoapp/EditActivity.java
@@ -2,11 +2,16 @@ package com.animesh.demoapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.util.TreeSet;
 
 public class EditActivity extends AppCompatActivity {
 
@@ -18,12 +23,42 @@ public class EditActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_activity);
         editText = (EditText) findViewById(R.id.editNote);
+        Intent intent = getIntent();
+
+        final int noteId = intent.getIntExtra("noteId",-1);
+        if (noteId != -1){
+            editText.setText(NotesApp.notesArrayList.get(noteId));
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                NotesApp.notesArrayList.set(noteId,String.valueOf(charSequence));
+                NotesApp.arrayAdapter.notifyDataSetChanged();
+             //   NotesApp.entryArrayList.set(noteId,String.valueOf(charSequence));
+                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.animesh.demoapp",0);
+                TreeSet<String> treeSet = new TreeSet<>(NotesApp.notesArrayList);
+                sharedPreferences.edit().putStringSet("notes",treeSet).apply();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
 
 
     }
 
 
-    public void onBackPressed() {
+/*    public void onBackPressed() {
         Intent intent = new Intent();
         Log.i("EntryBefore", editText.getText().toString());
         intent.putExtra("Entry", editText.getText().toString());
@@ -31,5 +66,5 @@ public class EditActivity extends AppCompatActivity {
         Log.i("EntryBefore", s);
         setResult(Activity.RESULT_OK,intent);
         finish();
-    }
+    }*/
 }
--- app/src/main/java/com/animesh/demoapp/NotesApp.java
+++ app/src/main/java/com/animesh/demoapp/NotesApp.java
@@ -1,8 +1,10 @@
 package com.animesh.demoapp;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,37 +18,69 @@ import android.widget.ListView;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 public class NotesApp extends AppCompatActivity {
 
     ListView listView;
-    ArrayAdapter<String> arrayAdapter;
-    ArrayList<String> notesArrayList;
-    ArrayList<String> entryArrayList;
+    static  ArrayAdapter<String> arrayAdapter;
+    static ArrayList<String> notesArrayList;
     Intent intent;
-    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        sharedPreferences = this.getSharedPreferences("com.animesh.demoApp", 0);
-
-        EditActivity editActivity = new EditActivity();
-        entryArrayList =new ArrayList<>();
-
         listView = (ListView) findViewById(R.id.notesList);
         notesArrayList = new ArrayList<String>();
-        notesArrayList.add("Example Note");
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, notesArrayList);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.animesh.demoApp", 0);
+        TreeSet<String> treeSet =(TreeSet<String>) sharedPreferences.getStringSet("notes",null);
+        if(treeSet == null){
+            notesArrayList.add("Example Note");
+        }else{
+            notesArrayList = new ArrayList<>(treeSet);
+        }
+
         listView.setAdapter(arrayAdapter);
         listView.setClickable(true);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 intent = new Intent(getApplicationContext(), EditActivity.class);
+                intent.putExtra("noteId",i);
                 startActivityForResult(intent,1);
 
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> adapterView, View view, final int i, long l) {
+                if (i != -1) {
+                    AlertDialog.Builder delete_entry = new AlertDialog.Builder(NotesApp.this)
+                            .setTitle("Delete entry")
+                            .setMessage("Are you sure you want to delete this entry?");
+
+                    delete_entry.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            notesArrayList.remove(i);
+                            listView.invalidateViews();
+                            arrayAdapter.notifyDataSetChanged();
+                            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.animesh.demoapp",0);
+                            TreeSet<String> treeSet = new TreeSet<>(NotesApp.notesArrayList);
+                            sharedPreferences.edit().putStringSet("notes",treeSet).apply();
+                        }
+                    })
+
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                    return true;
+
+                }
+                return false;
             }
         });
 
@@ -74,7 +108,7 @@ public class NotesApp extends AppCompatActivity {
         return false;
     }
 
-    @Override
+/*    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
         super.onActivityResult(requestCode, resultCode, data);
@@ -92,5 +126,5 @@ public class NotesApp extends AppCompatActivity {
 
             }
         }
-    }
+    }*/
 }
